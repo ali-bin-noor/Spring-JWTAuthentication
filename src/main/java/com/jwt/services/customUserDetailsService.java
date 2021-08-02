41b4b93788472525b668123f692ae5ab24f65b03
Maheboob Ali Kankurti <mahebok1@amdocs.com>
2021-08-02 08:47:35 +0530

    added the configuratiion.class with authentication management ant jwt utill helper class

---
+++ src/main/java/com/jwt/services/customUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.jwt.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class customUserDetailsService implements UserDetailsService
+{
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        if (username.equals("Durgesh"))
+        {
+            return new User("Durgesh","Durgesh123",new ArrayList<>());
+        }
+        else
+        {
+            throw new UsernameNotFoundException("User Not found");
+
+        }
+    }
+}
